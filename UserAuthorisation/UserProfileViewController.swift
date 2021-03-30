159f2cecdfaf513bbc4f26645276f60dbee1755c
st3lss <st3ls@yandex.ru>
2021-03-30 19:15:39 +0300

    set almost whole

---
+++ UserAuthorisation/UserProfileViewController.swift
@@ -0,0 +1,28 @@
+//
+//  UserProfileViewController.swift
+//  UserAuthorisation
+//
+//  Created by Tamerlan on 30.03.2021.
+//
+
+import UIKit
+
+class UserProfileViewController: UIViewController {
+
+    @IBOutlet var userNameProfileTF: UITextField!
+    @IBOutlet var logOutButton: UIButton!
+    
+    var userNameProfile: String!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        userNameProfileTF.text = userNameProfile
+    }
+    
+  
+    
+    @IBAction func logOutActionButton() {
+        dismiss(animated: true)
+    }
+    
+}
--- UserAuthorisation/ViewController.swift
+++ UserAuthorisation/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var userNameTF: UITextField!
+    @IBOutlet var passwordTF: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let userProfileVC = segue.destination as? UserProfileViewController else { return }
+        userProfileVC.userNameProfile = userNameTF.text
+    }
 
+    @IBAction func logInButton() {
+//        if userNameTF.text == "test" && passwordTF.text == "1234" {
+//            
+//        } else {
+//            print("")
+//        }
+    }
+    
 }
 
